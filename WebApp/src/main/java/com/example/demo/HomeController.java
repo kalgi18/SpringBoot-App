ff4644c678a023b3b5e4b76ad0ac4294d130bec7
Kalgi Shah <60284672+kalgi18@users.noreply.github.com>
2021-12-05 16:07:08 +0530

    Add files via upload

---
+++ WebApp/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("home")
+	public String home() {
+		System.out.println("Hi");
+		return "home.jsp";
+	}
+}
